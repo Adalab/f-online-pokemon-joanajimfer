4f4b76574da7c6ce7adbfc046c3ac991c8633f65
joanajimfer <joanajimfer@gmail.com>
2019-03-11 20:33:48 +0100

    create docs

---
+++ pokemon-jjf/docs/precache-manifest.305a094103486abb77bec4d0c7c20dd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "827e4bdfbc4cca86bd6f",
+    "url": "/static/css/main.de7fa48c.chunk.css"
+  },
+  {
+    "revision": "827e4bdfbc4cca86bd6f",
+    "url": "/static/js/main.9bebc89d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "7d1851d81603a87ff103",
+    "url": "/static/js/2.ea4d1651.chunk.js"
+  },
+  {
+    "revision": "fb8c81d97d89ca315f8e52068098be71",
+    "url": "/index.html"
+  }
+];
