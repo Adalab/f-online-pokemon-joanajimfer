082b49ec4c0cc9a0c5b8cd0267bb3e95283dd4cc
joanajimfer <joanajimfer@gmail.com>
2019-03-11 13:40:08 +0100

    Add styles to the pokemon list

--- pokemon-jjf/src/components/PokemonList/index.js
+++ pokemon-jjf/src/components/PokemonList/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './PokemonList.css';
 
 class PokemonList extends React.Component {
     render() {
@@ -11,20 +12,22 @@ class PokemonList extends React.Component {
         }
         else {
             return (
-                <section>
-                    <ul>
+                <section className='pokemonList__section'>
+                    <ul className='pokemonList__list'>
                         {pokemonsData.map((pokemon, id) => {
                             return (
-                                <li key={id}>
-                                    <h3>{pokemon.name}</h3>
-                                    <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-                                    <p>ID / {pokemon.id}</p>
-                                    <ul>{pokemon.types.map((type, id) => {
+                                <li className='pokemonList__card' key={id}>
+                                    <h3 className='pokemonList__name'>{pokemon.name}</h3>
+                                    <img src={pokemon.sprites.front_default} alt={pokemon.name} className='pokemonList__img' />
+                                    <div className='pokemonList__data'>
+                                    <p className='pokemonList__data--number'># {pokemon.id}</p>
+                                    <ul className='pokemonList__data--types'>{pokemon.types.map((type, id) => {
                                         return (
                                             <li key={id}>{type.type.name}</li>
                                         )
                                     })}
                                     </ul>
+                                    </div>
                                 </li>  
             )})}
                     </ul>
