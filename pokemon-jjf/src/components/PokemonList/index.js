3bb062573bc92d9cfe0830ecce6326aa55b5180e
joanajimfer <joanajimfer@gmail.com>
2019-03-11 11:50:26 +0100

    Format pokemonlist

--- pokemon-jjf/src/components/PokemonList/index.js
+++ pokemon-jjf/src/components/PokemonList/index.js
@@ -2,36 +2,33 @@ import React from 'react';
 
 class PokemonList extends React.Component {
     render() {
-        console.log('props', this.props)
-        if(this.props.fetchingState === true) {
+        const { fetchingState, pokemonsData } = this.props;
+
+        if (fetchingState === true) {
             return (
                 <p>Loading</p>
             )
-        } else {
-        return (
-            <section>
-            <h2>Pokemon encontrados:</h2>
-         <ul>
-             {this.props.pokemonsData.map((pokemon, id) => {
-               return (
-                 <li key={id}>
-                 <h3>{pokemon.name}</h3>
-                 <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-                 <p>ID / {pokemon.id}</p>
-                 <ul>{pokemon.types.map((type, id) => {
-                  return(
-                     <li key={id}>{type.type.name}</li>
-                   
-                 )})}
-                 </ul>
-                 </li>
-                 
-               )
-             })}
-           </ul> 
-          </section>
-        )}
-    }
-}
+        }
+        else {
+            return (
+                <section>
+                    <ul>
+                        {pokemonsData.map((pokemon, id) => {
+                            return (
+                                <li key={id}>
+                                    <h3>{pokemon.name}</h3>
+                                    <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                                    <p>ID / {pokemon.id}</p>
+                                    <ul>{pokemon.types.map((type, id) => {
+                                        return (
+                                            <li key={id}>{type.type.name}</li>
+                                        )
+                                    })}
+                                    </ul>
+                                </li>  
+            )})}
+                    </ul>
+                </section>
+            )}}}
 
 export default PokemonList;
